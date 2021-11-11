ca2dfd211addfa8efc499295d42ecd1101366391
DarkoDrljaca <darkodrljaca@gmail.com>
2021-11-11 20:43:46 +0100

    A deep comparison of the two objects

---
+++ deep-comparison.js
@@ -0,0 +1,27 @@
+// A deep comparison of the two objects:
+let person1 = {
+    ["firstName"] : 'Nick',
+    ["lastName" ]: 'Jackson'
+}
+let person2 = {
+    ["firstName"] : 'Nick',
+    ["lastName" ]: 'Jackson'
+}
+function checkSimilarity(object1, object2) {
+    let arr1 = Object.keys(object1);
+    let arr2 = Object.keys(object2);
+    let message = "";
+    if(arr1.length === arr2.length) {
+        for(let i = 0; i < arr1.length; i++) {
+            if(arr1[i] === arr2[i] && object1[arr1[i]] === object2[arr2[i]]) {                
+                message = 'They are equal.'
+            } else {
+                message = "They are not equal.";
+            }
+        }
+    }
+    return message;            
+}
+
+let comapreResult = checkSimilarity(person1, person2);
+console.log(comapreResult);
